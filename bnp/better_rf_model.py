537e56b2df8518c9fd334ecd267f2ebecf10924d
Benjamin Pachev <benjaminpachev@gmail.com>
2016-04-11 23:09:14 -0600

    fixed bug.

--- bnp/better_rf_model.py
+++ bnp/better_rf_model.py
@@ -8,7 +8,7 @@ train_features, train_labels, test_features, ids, outfile = read_data()
 
 model = RandomForestClassifier(n_estimators=1000, criterion= "entropy",n_jobs=-1, max_depth = 35, min_samples_split=4, min_samples_leaf = 2)
 for i in xrange(5):
-    probs, tprobs = cross_val_model(model, train_features, train_labels, test_features)
+    probs, tprobs = cross_val_model(model, train_features, train_labels, test_features, return_trainprobs=True)
     np.savez(outfile+"_feature"+str(i), train=tprobs, test=probs)
 #save_submission(outfile, ids=ids, probs=probs)
 #print "saved model, validating now"
